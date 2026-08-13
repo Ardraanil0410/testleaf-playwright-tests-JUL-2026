92c860cf813d77b31f2f3742daa837dd0c63f645
Ardra Anil <ardraanil@mac.home>
2026-08-13 12:24:21 -0300

    Learning OOPS

---
+++ tests/myPractice/07_ConsolidatingUntilClassConcepts.js
@@ -0,0 +1,36 @@
+"use strict";
+class TestCase {
+    testName;
+    testerName;
+    executionTime;
+    status;
+    constructor(testName, testerName, executionTime, status) {
+        this.testName = testName;
+        this.testerName = testerName;
+        this.executionTime = executionTime;
+        this.status = status;
+    }
+    displayTestDetails() {
+        console.log(`Test Name:${this.testName}`);
+        console.log(`Tester:${this.testerName}`);
+        console.log(`Execution Time:${this.executionTime}`);
+        console.log(`Status:${this.status}`);
+    }
+    checkStatus() {
+        if (this.status === "Passed") {
+            console.log("Test completed successfully");
+        }
+        else if (this.status === "Failed") {
+            console.log("Test needs investigation");
+        }
+        else {
+            console.log("Test cannot be executed");
+        }
+    }
+}
+let testdetails = new TestCase("Login Test", "Ardra", 5, "Passed");
+testdetails.displayTestDetails();
+testdetails.checkStatus();
+let testdetails2 = new TestCase("Payment Test", "Latheesh", 8, "Failed");
+testdetails2.displayTestDetails();
+testdetails2.checkStatus();
---
+++ tests/myPractice/07_ConsolidatingUntilClassConcepts.ts
@@ -0,0 +1,43 @@
+type TestStatus ="Passed"|"Failed"|"Blocked"
+class TestCase{
+    testName:string
+    testerName:string
+    executionTime:number
+    status:TestStatus
+    
+constructor(testName:string,testerName:string,executionTime:number,status:TestStatus)
+{
+    
+   this.testName=testName
+    this.testerName=testerName
+    this.executionTime=executionTime
+    this.status=status
+}
+displayTestDetails(){
+    console.log(`Test Name:${this.testName}`);
+    console.log(`Tester:${this.testerName}`);
+    console.log(`Execution Time:${this.executionTime}`);
+    console.log(`Status:${this.status}`);
+}
+
+checkStatus(){
+    if (this.status==="Passed") {
+        console.log("Test completed successfully");   
+    }
+    else if(this.status==="Failed"){
+        console.log("Test needs investigation");    
+    }
+    else{
+        console.log("Test cannot be executed");   
+    }
+}   
+
+}
+let testdetails=new TestCase("Login Test","Ardra",5,"Passed")
+testdetails.displayTestDetails()
+testdetails.checkStatus()
+
+let testdetails2=new TestCase("Payment Test","Latheesh",8,"Failed")
+testdetails2.displayTestDetails()
+testdetails2.checkStatus()
+
